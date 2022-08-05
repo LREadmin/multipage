73023c8f52ec41984e0bb023c3f8a9bc9d6925ee
LREadmin <109240880+LREadmin@users.noreply.github.com>
2022-08-05 15:24:05 -0600

    Add files via upload

--- Hello.py
+++ Hello.py
@@ -8,4 +8,14 @@ st.set_page_config(
 
 st.write("# Welcome to Denver Water's Climate App! 👋")
 
+st.header("SNOTEL Data")
+"""
+*WY = Water Year; October 1 through September 30; e.g. WY 2021 begins October 1, 2020 and ends September 30, 2021
+
+*Peak SWE Day = Calendar Day (January 1 = Day 1) of last occurence of Peak SWE value
+
+*POR = Period of Record
+"""
+
 st.sidebar.success("Select a demo above.")
+
