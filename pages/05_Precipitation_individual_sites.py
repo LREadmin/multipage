205964f15bb7d068f0d41d14987040987ea97596
Micaela Pedrazas <67972197+mpedrazah@users.noreply.github.com>
2022-08-17 14:15:03 -0500

    Update 05_Precipitation_individual_sites.py

--- pages/05_Precipitation_individual_sites.py
+++ pages/05_Precipitation_individual_sites.py
@@ -174,7 +174,7 @@ sumStats=sumStats.rename({1:'Median',2:'Trend'})
 sumStats = sumStats[cols] 
 
 #%%colormap
-def background_gradient(s, m=None, M=None, cmap='Blues', low=-10, high=0):
+def background_gradient(s, m=None, M=None, cmap='Blues', low=0, high=0):
     if m is None:
         m = s.min().min()
     if M is None:
