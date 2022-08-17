01698e57dfa29c371b25fd8dff07a96a44d6fee3
Micaela Pedrazas <67972197+mpedrazah@users.noreply.github.com>
2022-08-17 09:43:09 -0500

    threshold default for compare sites

--- pages/06_Precipitation_compare_sites.py
+++ pages/06_Precipitation_compare_sites.py
@@ -201,9 +201,9 @@ start_date1=pandas.to_datetime(start_date)
 end_date1=pandas.to_datetime(end_date) 
 
 #%%threshold filter
-thresholdHigh = st.sidebar.number_input('Set Upper %s threshold:'%stat_select,step=1)
+thresholdHigh = st.sidebar.number_input('Set Upper %s threshold:'%stat_select,step=1,min_value=0, value=100)
 
-thresholdLow = st.sidebar.number_input('Set Lower %s threshold:'%stat_select,step=1)
+thresholdLow = st.sidebar.number_input('Set Lower %s threshold:'%stat_select,step=1,min_value=0, value=0)
 
 #%%FILTERED DATA
 data_sites_years=data_sites[(data_sites['date']>start_date1)&(data_sites['date']<=end_date1)]
