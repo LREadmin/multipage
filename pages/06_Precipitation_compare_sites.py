1a0f582a12c8cd3f1a6f47bd5ba6686c885c16bb
Micaela Pedrazas <67972197+mpedrazah@users.noreply.github.com>
2022-08-24 01:13:19 -0500

    Update 06_Precipitation_compare_sites.py

--- pages/06_Precipitation_compare_sites.py
+++ pages/06_Precipitation_compare_sites.py
@@ -45,7 +45,7 @@ data['CY']=data['date'].dt.year
 
 #%%select months
 
-monthOptions=pandas.DataFrame({'Month':['Jan','Feb','Mar','A','M','J','Jul','Aug','Sep','Oct','Nov','Dec'],
+monthOptions=pandas.DataFrame({'Month':['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'],
                                'Season':['Winter','Winter','Winter','Spring','Spring','Spring','Summer','Summer','Summer','Fall','Fall','Fall'],
                                'Num':[1,2,3,4,5,6,7,8,9,10,11,12]})
 monthSelect=monthOptions['Month']
