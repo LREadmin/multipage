a49bbc46dc6f78c4aaa46a1759f1d2d8b2fa9fee
Micaela Pedrazas <67972197+mpedrazah@users.noreply.github.com>
2022-09-09 17:18:37 -0500

    bwr reverse cmap

--- pages/06_Precipitation_compare_sites.py
+++ pages/06_Precipitation_compare_sites.py
@@ -360,7 +360,7 @@ for n in list:
     yearList[n]=temp2
 
 #%%colormap
-def background_gradient(s, m=None, M=None, cmap='bwr',low=0, high=0):
+def background_gradient(s, m=None, M=None, cmap='bwr_r',low=0, high=0):
     #print(s.shape)
     if m is None:
         m = s.min().min()
@@ -413,7 +413,7 @@ for n in list:
     yearList[n]=temp2
 
 #%%colormap
-def background_gradient(s, m=None, M=None, cmap='bwr',low=0, high=0):
+def background_gradient(s, m=None, M=None, cmap='bwr_r',low=0, high=0):
     #print(s.shape)
     if m is None:
         m = s.min().min()
@@ -465,7 +465,7 @@ for n in list:
     yearList[n]=temp2
 
 #%%colormap
-def background_gradient(s, m=None, M=None, cmap='bwr',low=0, high=0):
+def background_gradient(s, m=None, M=None, cmap='bwr_r',low=0, high=0):
     #print(s.shape)
     if m is None:
         m = s.min().min()
