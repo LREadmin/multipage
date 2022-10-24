3c1107b5a78bbb4d348b6acf5fe41b69b0e73bef
LREadmin <109240880+LREadmin@users.noreply.github.com>
2022-10-24 07:29:45 -0600

    filter out values >100%

--- pages/08_Soil_Moisture_individual_sites.py
+++ pages/08_Soil_Moisture_individual_sites.py
@@ -22,7 +22,6 @@ import numpy as np
 import matplotlib.pyplot as plt #for plotting
 from matplotlib import colors #for additional colors
 
-import pymannkendall as mk #for trend anlaysis
 #%% Define data download as CSV function
 #functions
 @st.cache
@@ -76,9 +75,20 @@ elementDF=pd.DataFrame({0:["SMS:-2:value","SMS:-4:value", "SMS:-8:value","SMS:-2
 
 container=st.sidebar.container()
 paramsSelect=elementDF['long']
-element_select=container.multiselect('Select depth(s):',paramsSelect,default=elementDF['long'])
+ 
+all=st.sidebar.checkbox("Select all")
+if all:
+    element_select = container.multiselect('Select one or more depths:', elementDF['long'], elementDF['long'])
+    
+else:
+    element_select=container.multiselect('Select depth(s):',paramsSelect,default=elementDF['long'])
+    
 element_select=elementDF.loc[elementDF['long'].isin(element_select)][0]
+
 elementStr= ','.join(element_select)
+
+if len(element_select)==0:
+    st.sidebar.error("Select at least one depth")
 
 #03 Select Water Year
 start_date = "%s-%s-0%s"%(startY,startM,startD) 
@@ -109,106 +119,119 @@ url=base+part1+site+por+element+part2
 
 s=requests.get(url).text
 
-urlData=pd.read_csv(url,header=headerCount.iloc[0],delimiter=',',error_bad_lines=False)
+urlDataRaw=pd.read_csv(url,header=headerCount.iloc[0],delimiter=',',error_bad_lines=False)#headerCount.iloc[0]
+
+#filter out data >100%
+datecol=urlDataRaw['Date']
+cols=urlDataRaw.columns[1:]
+urlData=urlDataRaw[cols].applymap(lambda x: np.nan if x > 100 else x)
+urlData['Date']=datecol
+cols2=urlData.columns.tolist()
+cols2 = [cols2[-1]]+cols2[:-1] 
+urlData=urlData.reindex(columns=cols2)
+
 #%% Download Daily Soil Moisture Data
 
 urlData['year']=pd.DatetimeIndex(urlData['Date']).year
 urlData['month']=pd.DatetimeIndex(urlData['Date']).month
-urlData['WY']= urlData.apply(lambda x: convert_to_WY(x), axis=1)
 
-#filter by WY
-dateFiltered=urlData[(urlData['WY']>=startYear)&(urlData['WY']<=endYear)]
-
-
-st.header("Soil Moisture Percent (pct) Start of Day Values")
-
-dateFiltered.set_index('Date')
-
-csv = convert_df(dateFiltered)
-st.download_button(
-     label="Download Daily Soil Moisture Data",
-     data=csv,
-     file_name='SMS_data.csv',
-     mime='text/csv',
- )
-
-# st.header("URL to download directly from NRCS")
-# url
-
-#%% Create pivot table using average soil moisture and show medians by WY
-dateFiltered['averageSoilMoisture']=(dateFiltered[urlData.columns[1:-3]]).mean(axis=1)
-dateFiltered_nonans = dateFiltered.dropna(subset=['averageSoilMoisture'])
-
-#filter by months with days > 25 that have average soil moisture data 
-smData=dateFiltered_nonans.groupby(['month','WY']).filter(lambda x : len(x)>=dayCountThres)
-
-pvTable=pd.pivot_table(smData, values=['averageSoilMoisture'],index='WY', columns={'month'},aggfunc=np.nanmedian, margins=False, margins_name='Total')
-pvTable=pvTable["averageSoilMoisture"].head(len(pvTable))
-
-pvTable=pvTable.rename(columns = months)
-
-#display pivot table 
-tableData=pvTable.style\
-    .set_properties(**{'width':'10000px','color':'white'})\
-    .apply(background_gradient, axis=None)\
-    .format(precision=1)
-
-st.dataframe(tableData)
-
-#download pivot table
-csv = convert_df(pvTable)
-st.download_button(
-     label="Download Table Data as CSV",
-     data=csv,
-     file_name='Median_SoilMoisture_byWY.csv',
-     mime='text/csv',
- )
-
-
-#%% Statistics Table
-
-medianTable=pvTable.median()
-medianTable=medianTable.to_frame('Median')
-medianTable=medianTable.transpose()
-
-#calculate trends using data that has no nans and count of days > 25
-trendData=smData[['averageSoilMoisture','month','WY']]
-trendData=trendData.set_index('WY').sort_index(ascending=True)
-months_list=trendData.month.unique()
-# trendData=trendData.set_index('month')
-manK=[]
-for i in months_list:
-    try:
-        print(str(i))
-        tempMK=mk.original_test(trendData[trendData.month==i][['averageSoilMoisture']])
-        print(tempMK)
-        if tempMK[2]>0.1:
+if len(urlData)==0:
+    "no data for this depth"
+else:
+    urlData['WY']= urlData.apply(lambda x: convert_to_WY(x), axis=1)
+    
+    #filter by WY
+    dateFiltered=urlData[(urlData['WY']>=startYear)&(urlData['WY']<=endYear)]
+    
+    dateFiltered.set_index('Date')
+    
+    st.header("Soil Moisture Percent (pct) Start of Day Values")
+    "Note: Soil moisture percent > 100% excluded"
+    
+    csv = convert_df(dateFiltered)
+    st.download_button(
+         label="Download Daily Soil Moisture Data",
+         data=csv,
+         file_name='SMS_data.csv',
+         mime='text/csv',
+     )
+    
+    # st.header("URL to download directly from NRCS")
+    # url
+    
+    #%% Create pivot table using average soil moisture and show medians by WY
+    dateFiltered['averageSoilMoisture']=(dateFiltered[urlData.columns[1:-3]]).mean(axis=1)
+    dateFiltered_nonans = dateFiltered.dropna(subset=['averageSoilMoisture'])
+        
+    #filter by months with days > 25 that have average soil moisture data 
+    smData=dateFiltered_nonans.groupby(['month','WY']).filter(lambda x : len(x)>=dayCountThres)
+    
+    pvTable=pd.pivot_table(smData, values=['averageSoilMoisture'],index='WY', columns={'month'},aggfunc=np.nanmedian, margins=False, margins_name='Total')
+    pvTable=pvTable["averageSoilMoisture"].head(len(pvTable))
+    
+    pvTable=pvTable.rename(columns = months)
+    
+    #display pivot table 
+    tableData=pvTable.style\
+        .set_properties(**{'width':'10000px','color':'white'})\
+        .apply(background_gradient, axis=None)\
+        .format(precision=1)
+    
+    st.dataframe(tableData)
+    
+    #download pivot table
+    csv = convert_df(pvTable)
+    st.download_button(
+         label="Download Table Data as CSV",
+         data=csv,
+         file_name='Median_SoilMoisture_byWY.csv',
+         mime='text/csv',
+     )
+    
+    
+    #%% Statistics Table
+    
+    medianTable=pvTable.median()
+    medianTable=medianTable.to_frame('Median')
+    medianTable=medianTable.transpose()
+    
+    #calculate trends using data that has no nans and count of days > 25
+    trendData=smData[['averageSoilMoisture','month','WY']]
+    trendData=trendData.set_index('WY').sort_index(ascending=True)
+    months_list=trendData.month.unique()
+    # trendData=trendData.set_index('month')
+    manK=[]
+    for i in months_list:
+        try:
+            print(str(i))
+            tempMK=mk.original_test(trendData[trendData.month==i][['averageSoilMoisture']])
+            print(tempMK)
+            if tempMK[2]>0.1:
+                manK.append(float('nan'))
+            else:
+                manK.append(tempMK[7])  
+        except:
             manK.append(float('nan'))
-        else:
-            manK.append(tempMK[7])  
-    except:
-        manK.append(float('nan'))
-
-manKdf=pd.DataFrame(manK,columns={'Trend'}).transpose()
-manKdf.columns=[months[x] for x in months_list]
-
-medianTableData=medianTable.append(manKdf)
-
-#display pivot table 
-st.markdown("Trend (Theil-Sen Slope (inches/year) if Mann-Kendall trend test is significant (p-value <0.1); otherwise nan). Months with less than 25 days of data are not included in the analysis.")
-
-displayTableData=medianTableData.style\
-    .set_properties(**{'width':'10000px','color':'white'})\
-    .format(precision=2)
-
-st.dataframe(displayTableData)
-
-#download pivot table
-csv = convert_df(medianTableData)
-st.download_button(
-     label="Download Statistics Table Data as CSV",
-     data=csv,
-     file_name='StatisticsTablebyMonth.csv',
-     mime='text/csv',
- )
-
+    
+    manKdf=pd.DataFrame(manK,columns={'Trend'}).transpose()
+    manKdf.columns=[months[x] for x in months_list]
+    
+    medianTableData=medianTable.append(manKdf)
+    
+    #display pivot table 
+    st.markdown("Trend (Theil-Sen Slope (inches/year) if Mann-Kendall trend test is significant (p-value <0.1); otherwise nan). Months with less than 25 days of data are not included in the analysis.")
+    
+    displayTableData=medianTableData.style\
+        .set_properties(**{'width':'10000px','color':'black'})\
+        .format(precision=2)
+    
+    st.dataframe(displayTableData)
+    
+    #download pivot table
+    csv = convert_df(medianTableData)
+    st.download_button(
+         label="Download Statistics Table Data as CSV",
+         data=csv,
+         file_name='StatisticsTablebyMonth.csv',
+         mime='text/csv',
+     )
