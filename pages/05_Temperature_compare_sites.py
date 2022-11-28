8b6aeb3ee6388328d26c60b692d2286ff995ebe3
Micaela Pedrazas <mpedrazah@users.noreply.github.com>
2022-11-27 22:24:18 -0600

    Update 05_Temperature_compare_sites.py

--- pages/05_Temperature_compare_sites.py
+++ pages/05_Temperature_compare_sites.py
@@ -23,36 +23,6 @@ import matplotlib
 import numpy as np
 import matplotlib.pyplot as plt
 
-#%%
-def cmap_map(function, cmap):
-    """ Applies function (which should operate on vectors of shape 3: [r, g, b]), on colormap cmap.
-    This routine will break any discontinuous points in a colormap.
-    """
-    cdict = cmap._segmentdata
-    step_dict = {}
-    # Firt get the list of points where the segments start or end
-    for key in ('red', 'green', 'blue'):
-        step_dict[key] = list(map(lambda x: x[0], cdict[key]))
-    step_list = sum(step_dict.values(), [])
-    step_list = np.array(list(set(step_list)))
-    # Then compute the LUT, and apply the function to the LUT
-    reduced_cmap = lambda step : np.array(cmap(step)[0:3])
-    old_LUT = np.array(list(map(reduced_cmap, step_list)))
-    new_LUT = np.array(list(map(function, old_LUT)))
-    # Now try to make a minimal segment definition of the new LUT
-    cdict = {}
-    for i, key in enumerate(['red','green','blue']):
-        this_cdict = {}
-        for j, step in enumerate(step_list):
-            if step in step_dict[key]:
-                this_cdict[step] = new_LUT[j, i]
-            elif new_LUT[j,i] != old_LUT[j, i]:
-                this_cdict[step] = new_LUT[j, i]
-        colorvector = list(map(lambda x: x + (x[1], ), this_cdict.items()))
-        colorvector.sort()
-        cdict[key] = colorvector
-
-    return matplotlib.colors.LinearSegmentedColormap('colormap',cdict,1024)
 #%% Website display information
 st.set_page_config(page_title="Temperature Site Comparison", page_icon="📈")
 
@@ -379,23 +349,22 @@ for n in list:
     yearList[n]=temp2
 
 #%%colormap
-
-bwr_cmap = plt.get_cmap('bwr')
-light_bwr = cmap_map(lambda x: x/2 + 0.5, bwr_cmap)
-
-
-def background_gradient(s, m=None, M=None, cmap=light_bwr,low=0.2, high=0):
+from matplotlib.colors import ListedColormap
+def background_gradient(s, m=None, M=None, cmap='bwr',low=0.2, high=0):
     #print(s.shape)
     if m is None:
         m = s.min().min()
     if M is None:
         M = s.max().max()
     rng = M - m
-    norm = colors.CenteredNorm(vcenter=0,)
+    norm = colors.CenteredNorm(vcenter=0)
     normed = s.apply(norm)
 
     cm = plt.cm.get_cmap(cmap)
-    c = normed.applymap(lambda x: colors.rgb2hex(cm(x)))
+    my_cmap = cm(np.arange(cm.N))
+    my_cmap[:, -1] = np.linspace(0, 1, cm.N)
+    my_cmap = ListedColormap(my_cmap)
+    c = normed.applymap(lambda x: colors.rgb2hex(my_cmap(x)))
     ret = c.applymap(lambda x: 'background-color: %s' % x)
     return ret 
 
