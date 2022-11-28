d4cc774c25912d2eba20f413efb93d40e9cfbff1
Micaela Pedrazas <mpedrazah@users.noreply.github.com>
2022-11-27 22:08:49 -0600

    Update 07_Precipitation_compare_sites.py

--- pages/07_Precipitation_compare_sites.py
+++ pages/07_Precipitation_compare_sites.py
@@ -378,7 +378,7 @@ for n in list:
 
 
 
-def background_gradient(s, m=None, M=None, cmap='bwr_r',low=0.2, high=0):
+def background_gradient(s, m=None, M=None, cmap='bwr_r',low=0., high=0.8):
     #print(s.shape)
     if m is None:
         m = s.min().min()
@@ -430,7 +430,7 @@ for n in list:
     yearList[n]=temp2
 
 #%%colormap
-def background_gradient(s, m=None, M=None, cmap='bwr_r',low=0.2, high=0):
+def background_gradient(s, m=None, M=None, cmap='bwr_r',low=0., high=0.8):
     #print(s.shape)
     if m is None:
         m = s.min().min()
