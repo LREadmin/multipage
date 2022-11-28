ed53fdcb96050f4716f06762951918b260953476
Micaela Pedrazas <mpedrazah@users.noreply.github.com>
2022-11-27 22:50:02 -0600

    Update 05_Temperature_compare_sites.py

--- pages/05_Temperature_compare_sites.py
+++ pages/05_Temperature_compare_sites.py
@@ -350,58 +350,7 @@ for n in list:
 
 #%%colormap
 
-def hex_to_rgb(value):
-    '''
-    Converts hex to rgb colours
-    value: string of 6 characters representing a hex colour.
-    Returns: list length 3 of RGB values'''
-    value = value.strip("#") # removes hash symbol if present
-    lv = len(value)
-    return tuple(int(value[i:i + lv // 3], 16) for i in range(0, lv, lv // 3))
-
-
-def rgb_to_dec(value):
-    '''
-    Converts rgb to decimal colours (i.e. divides each value by 256)
-    value: list (length 3) of RGB values
-    Returns: list (length 3) of decimal values'''
-    return [v/256 for v in value]
-
-def get_continuous_cmap(hex_list):
-    ''' creates and returns a color map that can be used in heat map figures.
-        If float_list is not provided, colour map graduates linearly between each color in hex_list.
-        If float_list is provided, each color in hex_list is mapped to the respective location in float_list. 
-        
-        Parameters
-        ----------
-        hex_list: list of hex code strings
-        float_list: list of floats between 0 and 1, same length as hex_list. Must start with 0 and end with 1.
-        
-        Returns
-        ----------
-        colour map'''
-    rgb_list = [rgb_to_dec(hex_to_rgb(i)) for i in hex_list]
-    float_list = list(np.linspace(0,1,len(rgb_list)))
-        
-    cdict = dict()
-    for num, col in enumerate(['red', 'green', 'blue']):
-        col_list = [[float_list[i], rgb_list[i][num], rgb_list[i][num]] for i in range(len(float_list))]
-        cdict[col] = col_list
-    cmp = matplotlib.colors.LinearSegmentedColormap('my_cmp', segmentdata=cdict, N=256)
-    return cmp
-
-
-hex_list = ['#0080ff',
-'#66b3ff',
-'#ccccff',
-'#ffcccc',
-'#ff6666',
-'#ff0000']
-
-
-new_cmap=get_continuous_cmap(hex_list)
-
-def background_gradient(s, m=None, M=None, cmap=new_cmap,low=0, high=0):
+def background_gradient(s, m=None, M=None, cmap='bwr',low=0, high=0):
     #print(s.shape)
     if m is None:
         m = s.min().min()
@@ -411,9 +360,8 @@ def background_gradient(s, m=None, M=None, cmap=new_cmap,low=0, high=0):
     norm = colors.CenteredNorm(vcenter=0)
     normed = s.apply(norm)
 
-  #  cm = plt.cm.get_cmap(cmap)
-  
-    c = normed.applymap(lambda x: colors.rgb2hex(new_cmap(x)))
+    cm = plt.cm.get_cmap(cmap)
+    c = normed.applymap(lambda x: colors.rgb2hex(cm(x)))
     ret = c.applymap(lambda x: 'background-color: %s' % x)
     return ret 
 
@@ -453,7 +401,7 @@ for n in list:
     yearList[n]=temp2
 
 #%%colormap
-def background_gradient(s, m=None, M=None, cmap=new_cmap,low=0.2, high=0):
+def background_gradient(s, m=None, M=None, cmap='bwr',low=0.2, high=0):
     #print(s.shape)
     if m is None:
         m = s.min().min()
@@ -464,9 +412,8 @@ def background_gradient(s, m=None, M=None, cmap=new_cmap,low=0.2, high=0):
                             M + (rng * high))
     normed = s.apply(norm)
    
-    
-   #cm = plt.cm.get_cmap(cmap)
-    c = normed.applymap(lambda x: colors.rgb2hex(new_cmap(x)))
+    cm = plt.cm.get_cmap(cmap)
+    c = normed.applymap(lambda x: colors.rgb2hex(cm(x)))
     ret = c.applymap(lambda x: 'background-color: %s' % x)
     return ret 
 
