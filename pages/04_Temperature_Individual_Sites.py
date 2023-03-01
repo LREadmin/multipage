35e3970eda22b0b802e29fa9cddea53ca3cc854d
LREadmin <109240880+LREadmin@users.noreply.github.com>
2023-03-01 16:07:09 -0700

    Update 04_Temperature_Individual_Sites.py

--- pages/04_Temperature_Individual_Sites.py
+++ pages/04_Temperature_Individual_Sites.py
@@ -188,7 +188,7 @@ def background_gradient(s, m=None, M=None, cmap='OrRd', low=0, high=0):
     
 tableData=data4.style\
     .format(precision=1)\
-    .format({'Years':"{:.0%}"})\
+    .format({'Years':"{:,}"})\
     .set_properties(**{'width':'10000px'})\
     .apply(background_gradient, axis=None)
 
