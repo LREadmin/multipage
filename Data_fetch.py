10e63408176cac79750840bd28c12e323e2cc7c2
timothy-salazar <timothy.salazar@colorado.edu>
2023-07-06 11:45:02 -0600

    more comments and doc strings, removing some extraneous code

--- Data_fetch.py
+++ Data_fetch.py
@@ -17,6 +17,9 @@ import ulmo
 import pandas as pd
 import numpy as np
 
+### Global Variables ###
+# not necessarily good practice, but these might be updated in the future, and 
+# I'd rather have them be highly visible than buried in the code.
 WSDLURL='https://hydroportal.cuahsi.org/Snotel/cuahsi_1_1.asmx?WSDL'
 VARIABLE_CODE='SNOTEL:WTEQ_D'
 START_DATE='1950-10-01'
@@ -37,24 +40,20 @@ SITE_CODES = [
         'SNOTEL:1014_CO_SNTL',
         'SNOTEL:939_CO_SNTL']
 
-
 def snotel_fetch(site_code: str, 
                  end_date: str,
-                 verbose: bool=False):
+                 verbose: bool=False) -> pd.DataFrame:
     """ Input:
-            site_code:
-            end_date:
-            verbose:
+            site_code: str - the site code we want to pull data for
+            end_date: str - the latest date we want to pull data for
+            verbose: bool - whether we should print information to stdout 
         Output:
-            values_df:
+            values_df: pd.DataFrame - the 
     """
     if verbose:
         print(site_code, VARIABLE_CODE, START_DATE, end_date)
-    # I'm 99% sure this is unnecessary
-    # values_df = None
     try:
-        # can pull data out of this dict
-        #Request data from the server
+        # Request data from the server
         site_values = ulmo.cuahsi.wof.get_values(WSDLURL, 
                                                  site_code, 
                                                  VARIABLE_CODE, 
@@ -80,14 +79,14 @@ def snotel_fetch(site_code: str,
 def get_snotel_data(end_date: str, 
                     verbose: bool=False):
     """ Input:
-            end_date: str -
-            verbose: bool -
+            end_date: str - the latest date we want to pull data for
+            verbose: bool - whether we should print information to stdout 
         Output:
 
     """
     data_raw=pd.DataFrame()
-    siteNamesList=[]
-    siteNamesListCode=[]
+    # siteNamesList=[]
+    # siteNamesListCode=[]
     sites = ulmo.cuahsi.wof.get_sites(WSDLURL)
     sites_df = pd.DataFrame.from_dict(sites, orient='index').dropna()
     sites_df=sites_df.reset_index()
@@ -101,16 +100,19 @@ def get_snotel_data(end_date: str,
         temp['Site']=name
         
         data_raw=pd.concat([data_raw,temp])
-        siteNamesList.append(name)
-        siteNamesListCode.append([name,site_code])
+        # siteNamesList.append(name)
+        # siteNamesListCode.append([name,site_code])
             
     data_raw.rename({'datetime': 'Date', 'value': 'SWE_in', 'name':'Site'}, axis=1, inplace=True)
+    # Also commenting this out, because why not.
+    # with open ("siteNamesList.txt","w") as output:
+    #     output.write(str(siteNamesList))
 
-    with open ("siteNamesList.txt","w") as output:
-        output.write(str(siteNamesList))
-
-    siteNamesListCode=pd.DataFrame(siteNamesListCode)
-    siteNamesListCode.to_csv('siteNamesListCode.csv',index=False)
+    # An extra column was added since this script was made. The data in that
+    # column doesn't come from the API, so auto-generating it isn't feasible
+    # at the moment.  
+    # siteNamesListCode=pd.DataFrame(siteNamesListCode)
+    # siteNamesListCode.to_csv('siteNamesListCode.csv',index=False)
 
     data_raw.to_csv("SNOTEL_data_raw.csv.gz",index=False)
 
@@ -130,7 +132,6 @@ def convert_weather_data(verbose: bool=False):
         # This might break - add test later
         file['site']=file_name[:2]
         weather=pd.concat([weather,file])
-        
     weather.to_csv("DW_weather.csv.gz",index=False)
 
 def main(args: argparse.Namespace):
@@ -138,10 +139,7 @@ def main(args: argparse.Namespace):
             args: populated namespace from Argument.Parser.parse_args()
         Output:    
     """
-    # If you're going to specify these like this, why not just use a string?
-
     end_date = datetime.now().strftime('%Y-%m-%d')
-
     verbose = args.verbose
     if args.snotel:
         get_snotel_data(end_date, verbose)
