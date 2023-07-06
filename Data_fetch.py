42386cade69b6b43b39a3992341b9bb0ba37427a
timothy-salazar <timothy.salazar@colorado.edu>
2023-07-06 10:54:30 -0600

    added argument parser, fleshed out main

--- Data_fetch.py
+++ Data_fetch.py
@@ -11,52 +11,38 @@ Get data for DW tools
 # the code police - follow your bliss.
 # https://docs.streamlit.io/streamlit-community-cloud/get-started/deploy-an-app/app-dependencies#add-python-dependencies
 import os
-import sys
+import argparse
 from datetime import datetime
 import ulmo  
 import pandas
 import numpy
 
+WSDLURL='https://hydroportal.cuahsi.org/Snotel/cuahsi_1_1.asmx?WSDL'
+VARIABLE_CODE='SNOTEL:WTEQ_D'
+SITE_CODES = [
+        'SNOTEL:335_CO_SNTL',
+        'SNOTEL:938_CO_SNTL',
+        'SNOTEL:913_CO_SNTL',
+        'SNOTEL:415_CO_SNTL',
+        'SNOTEL:936_CO_SNTL',
+        'SNOTEL:1186_CO_SNTL',
+        'SNOTEL:485_CO_SNTL',
+        'SNOTEL:505_CO_SNTL',
+        'SNOTEL:1187_CO_SNTL',
+        'SNOTEL:531_CO_SNTL',
+        'SNOTEL:935_CO_SNTL',
+        'SNOTEL:970_CO_SNTL',
+        'SNOTEL:937_CO_SNTL',
+        'SNOTEL:1014_CO_SNTL',
+        'SNOTEL:939_CO_SNTL']
 
-#import and store site data for ALL Snotel Sites
-wsdlurl='https://hydroportal.cuahsi.org/Snotel/cuahsi_1_1.asmx?WSDL'
-variablecode='SNOTEL:WTEQ_D'
-site_codes = [
-    'SNOTEL:335_CO_SNTL',
-    'SNOTEL:938_CO_SNTL',
-    'SNOTEL:913_CO_SNTL',
-    'SNOTEL:415_CO_SNTL',
-    'SNOTEL:936_CO_SNTL',
-    'SNOTEL:1186_CO_SNTL',
-    'SNOTEL:485_CO_SNTL',
-    'SNOTEL:505_CO_SNTL',
-    'SNOTEL:1187_CO_SNTL',
-    'SNOTEL:531_CO_SNTL',
-    'SNOTEL:935_CO_SNTL',
-    'SNOTEL:970_CO_SNTL',
-    'SNOTEL:937_CO_SNTL',
-    'SNOTEL:1014_CO_SNTL',
-    'SNOTEL:939_CO_SNTL']
-
-# start and end dates needed for initial data fetch
-startY=1950
-startM=10
-startD=1
-start_date = "%s-%s-0%s"%(startY,startM,startD) #if start day is single digit, add leading 0
-end_date = datetime.now().strftime('%Y-%m-%d')
-
-sites = ulmo.cuahsi.wof.get_sites(wsdlurl)
-sites_df = pandas.DataFrame.from_dict(sites, orient='index').dropna()
-sites_df=sites_df.reset_index()
 
 def snotel_fetch(site_code, 
-                 variablecode=variablecode, 
-                 start_date=start_date, 
-                 end_date=end_date,
+                 start_date, 
+                 end_date,
                  verbose=False):
     """ Input:
             site_code:
-            variablecode:
             start_date:
             end_date:
             verbose:
@@ -64,15 +50,15 @@ def snotel_fetch(site_code,
             values_df:
     """
     if verbose:
-        print(site_code, variablecode, start_date, end_date)
+        print(site_code, VARIABLE_CODE, start_date, end_date)
     # I'm 99% sure this is unnecessary
     # values_df = None
     try:
         # can pull data out of this dict
         #Request data from the server
-        site_values = ulmo.cuahsi.wof.get_values(wsdlurl, 
+        site_values = ulmo.cuahsi.wof.get_values(WSDLURL, 
                                                  site_code, 
-                                                 variablecode, 
+                                                 VARIABLE_CODE, 
                                                  start=start_date, 
                                                  end=end_date)
         #Convert to a Pandas DataFrame   
@@ -87,20 +73,29 @@ def snotel_fetch(site_code,
     # Try/Excepts that don't specify what kind of error they're expecting cause
     # me physical pain
     except:
-        print("Unable to fetch %s" % variablecode)
+        print("Unable to fetch %s" % VARIABLE_CODE)
     # As written this function can return None
     return values_df
 
-# SITE info
 
-def get_site_info():
+def get_site_info(start_date,
+                  end_date):
+    """ Input:
+            start_date:
+            end_date:
+        Output:
+
+    """
     data_raw=pandas.DataFrame()
     siteNamesList=[]
     siteNamesListCode=[]
+    sites = ulmo.cuahsi.wof.get_sites(WSDLURL)
+    sites_df = pandas.DataFrame.from_dict(sites, orient='index').dropna()
+    sites_df=sites_df.reset_index()
 
-    for row in site_codes:
+    for row in SITE_CODES:
         print(row)
-        values_df = snotel_fetch(row, variablecode, start_date, end_date)
+        values_df = snotel_fetch(row, VARIABLE_CODE, start_date, end_date)
         temp=values_df[['datetime','value']].copy()
         name=sites_df['name'][sites_df['index']==row].iloc[0]
         temp['Site']=name
@@ -119,7 +114,12 @@ def get_site_info():
 
     data_raw.to_csv("SNOTEL_data_raw.csv.gz",index=False)
 
-def convert_weather_data():
+def convert_weather_data(verbose=False):
+    """ Input:
+
+        Output:
+
+    """
     wd=os.getcwd()
     # the backslash at the end defeated the purpose of using "os.path.join"
     path=os.path.join(wd,"Weather_Data") #put all DW files in one directory with nothing else
@@ -133,5 +133,41 @@ def convert_weather_data():
         
     weather.to_csv("DW_weather.csv.gz",index=False)
 
+def main(args: argparse.Namespace):
+    """ Input:
+            args: populated namespace from Argument.Parser.parse_args()
+        Output:
+
+            
+    """
+    verbose = args.verbose
+    if args.snotel:
+        pass
+    if args.weather:
+        pass
+
+    #import and store site data for ALL Snotel Sites
+
+    # If you're going to specify these like this, why not just use a string?
+    startY=1950
+    startM=10
+    startD=1
+    start_date = "%s-%s-0%s"%(startY,startM,startD) #if start day is single digit, add leading 0
+    end_date = datetime.now().strftime('%Y-%m-%d')
+
 if __name__ == "__main__":
-    pass
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', 
+                        '--snotel', 
+                        action='store_true',
+                        help='download the SNOTEL data')
+    parser.add_arguments('-w', 
+                        '--weather', 
+                        action='store_true',
+                        help='read the weather data from excel docs stored in dropbox.')
+    parser.add_arguments('-v', 
+                        '--verbose', 
+                        action='store_true',
+                        help='print a bunch of stuff to stdout - useful for debugging')
+    args = parser.parse_args()
+    main(args)
